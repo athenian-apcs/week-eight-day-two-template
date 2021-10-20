4ed319a2b03abb4be755020596fa3aef27cff405
Nathan Lin <nlin@athenian.org>
2021-10-20 08:25:25 -0700

    first draft

--- src/main/java/RationalClient.java
+++ src/main/java/RationalClient.java
@@ -9,16 +9,16 @@ public class RationalClient {
         System.out.println(r.numerator); // 1
         System.out.println(r.denominator); // 2
 
-        // A basic test for adding Rationals
-        Rational s = new Rational(1, 3);
-        System.out.println(s); // 1/3
-        Rational t = Rational.add(r, s);
-        System.out.println(t); // 5/6
-
-        // A more advanced test for adding Rational that won't work
-        // until the greatestCommonFactor and simplify methods work
-        Rational x = new Rational(1, 6);
-        Rational y = Rational.add(t, x);
-        System.out.println(y); // 1/1
+//        // A basic test for adding Rationals
+//        Rational s = new Rational(1, 3);
+//        System.out.println(s); // 1/3
+//        Rational t = Rational.add(r, s);
+//        System.out.println(t); // 5/6
+//
+//        // A more advanced test for adding Rational that won't work
+//        // until the greatestCommonFactor and simplify methods work
+//        Rational x = new Rational(1, 6);
+//        Rational y = Rational.add(t, x);
+//        System.out.println(y); // 1/1
     }
 }
